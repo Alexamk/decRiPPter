4dc07132ddf4e7d8e192b1587748e1830552f690
amkloosterman <a.m.kloosterman@biology.leidenuniv.nl>
2020-05-27 23:08:16 +0200

    Added error catching in the subprocesses, which are passed onto the main operator and raised there

--- lib/multiprocessing_wrappers.py
+++ lib/multiprocessing_wrappers.py
@@ -2,9 +2,11 @@
 
 import os
 import time
+import traceback
 
 from multiprocessing import Process,Queue
 from log import return_logger
+
 
 logger = return_logger(__name__, False)
 
@@ -12,6 +14,16 @@ def put_jobs(jobs,queue,nr):
     for job in jobs[0:nr]:
         queue.put(job)
     return jobs[nr:]
+    
+def clear_queue(queue):
+    while queue.qsize() > 0:
+        _ = queue.get()
+    queue.close()
+    
+def stop_processes(processes):
+    for process in processes:
+        process.terminate()
+        process.join()
 
 def operator(jobs,nr_workers,target_function,target_args,quiet=False,sleeptime=1,multiplier=5):
     
@@ -29,8 +41,11 @@ def operator(jobs,nr_workers,target_function,target_args,quiet=False,sleeptime=1
         w.start()
     results = []
     put_ends = False
+    counter_print = 0
+    print_status = False
+    multiplier_to_print = 100
     while any([w.is_alive() for w in workers]):
-        if not quiet:
+        if print_status:
             logger.debug('Working with target function: %s' %target_function.func_name)
             logger.debug('%i workers still alive' %(len([w.is_alive() for w in workers])))
             logger.debug('Work queue: %i; all_groups: %i' %(job_queue.qsize(),len(jobs)))
@@ -38,12 +53,16 @@ def operator(jobs,nr_workers,target_function,target_args,quiet=False,sleeptime=1
             res = results_queue.get()
             if res == None:
                 break
+            elif type(res) == ValueError:
+                logger.error('Encountered error - terminating')
+                stop_processes(workers)
+                clear_queue(job_queue)
+                raise res
             else:
                 results.append(res)
-                if not quiet:
-                    logger.debug(len(results))
-                    logger.debug('Remaining in q: %i' %results_queue.qsize())
-        if not quiet:
+                if print_status:
+                    logger.debug('New results. Remaining in q: %i' %results_queue.qsize())
+        if print_status:
             logger.debug('Continuing')
         if job_queue.qsize() < nr_workers:
             if len(jobs) > 0:
@@ -52,6 +71,12 @@ def operator(jobs,nr_workers,target_function,target_args,quiet=False,sleeptime=1
                 for _ in range(nr_workers):
                     job_queue.put(False)
                 put_ends = True
+        # Print only once every 10 times it checks the queue
+        counter_print += 1
+        if counter_print % multiplier_to_print == 0:
+            print_status = True
+        else:
+            print_status = False
         time.sleep(sleeptime)
     while not results_queue.empty():
         res = results_queue.get()
@@ -66,6 +91,7 @@ def operator(jobs,nr_workers,target_function,target_args,quiet=False,sleeptime=1
     return results
     
 def worker_wrapper(args,job_queue,results_queue,function,quiet):
+    process_id = os.getpid()
     if not quiet:
         logger.debug('Starting process id: %s'  %os.getpid())
     while True:
@@ -76,21 +102,20 @@ def worker_wrapper(args,job_queue,results_queue,function,quiet):
             continue
         if job == False:
             break
-        if not quiet:
-            if type(job) == str:
-                logger.debug('Process id %s starting work on job %s' %(os.getpid(),job))
-            elif type(job) in (tuple,list):
-                logger.debug('Process id %s starting work on job starting with %s' %(os.getpid(),job[0]))
-            elif type(job) == dict:
-                logger.debug('Process id %s starting work on job starting with %s' %(os.getpid(),job.keys()[0]))
+        if type(job) == str:
+            logger.debug('Process id %s starting work on job %s' %(process_id, job))
+        elif type(job) in (tuple,list):
+            logger.debug('Process id %s starting work on job starting with %s' %(process_id, job[0]))
+        elif type(job) == dict:
+            logger.debug('Process id %s starting work on job starting with %s' %(process_id, job.keys()[0]))
             
         job_args = [job] + args
-        result = function(*job_args)
-        results_queue.put(result)
-    if not quiet:
-        logger.debug('Process %s exiting' %os.getpid())
+        try:
+            result = function(*job_args)
+            results_queue.put(result)
+        except:
+            error = traceback.format_exc()
+            logger.error('Process id %s ran into an error' %process_id)
+            results_queue.put(ValueError('Process id %s ran into an error:\n%s' %(process_id, error)))
+    logger.debug('Process %s exiting' %os.getpid())
 
-        
-
-
-        
